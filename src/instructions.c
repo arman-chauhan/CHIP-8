9b24b6bf9864181c1fda24a5cb253a8bc6ca21d6
Arman chauhan <ar.chauhan9863@gmail.com>
2024-10-09 23:05:22 +0545

    Instructions done

---
+++ src/instructions.c
@@ -0,0 +1,234 @@
+#include "include/instructions.h"
+
+#include "include/chip8.h"
+
+void CLS(Chip8State *state) {
+    for (int i = 0; i < DISPLAY_HEIGHT; i++) {
+        for (int j = 0; j < DISPLAY_WIDTH; j++) { state->FB[i][j] = 0; }
+    }
+    state->PC += 2;
+}
+void RET(Chip8State *state) {
+    state->SP--;
+    state->PC = state->stack[state->SP];
+}
+void CALL(Chip8State *state, uint8_t *opCode) {
+    const uint16_t nnn = GET_NNN(opCode);
+    if (state->SP >= 16) {
+        perror("Error: STACK OVERFLOW");
+        state->running = 0;
+    }
+    // Push (PC + 2) to stack, since PC points to current CALL
+    state->stack[state->SP] = state->PC + 2;
+    state->SP++;
+    state->PC = nnn;
+}
+void JUMP_ABS(Chip8State *state, uint8_t *opCode) {
+    const uint16_t nnn = GET_NNN(opCode);
+    state->PC = nnn;
+}
+void JUMP_REL(Chip8State *state, uint8_t *opCode) {
+    const uint16_t nnn = GET_NNN(opCode);
+    state->PC = nnn + state->V[0];
+}
+
+void SKIP_EQ(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t kk = GET_KK(opCode);
+    if (state->V[x] == kk) state->PC += 2;
+    state->PC += 2;
+}
+void SKIP_NEQ(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t kk = GET_KK(opCode);
+    if (state->V[x] != kk) state->PC += 2;
+    state->PC += 2;
+}
+void SKIP_EQ_REG(Chip8State *state, uint8_t *opCdoe) {
+    const uint8_t x = opCdoe[0] & 0x0f;
+    const uint8_t y = opCdoe[1] >> 4;
+    if (state->V[x] == state->V[y]) state->PC += 2;
+    state->PC += 2;
+}
+void SKIP_NEQ_REG(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t y = GET_Y(opCode);
+    if (state->V[x] != state->V[y]) state->PC += 2;
+    state->PC += 2;
+}
+
+void LOAD(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t kk = GET_KK(opCode);
+    state->V[x] = kk;
+    state->PC += 2;
+}
+void MVI(Chip8State *state, uint8_t *opCode) {
+    const uint16_t nnn = GET_NNN(opCode);
+    state->I = nnn;
+    state->PC += 2;
+}
+void MOV(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t y = GET_Y(opCode);
+    state->V[x] = state->V[y];
+    state->PC += 2;
+}
+void MOVD(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    state->DT = state->V[x];
+    state->PC += 2;
+}
+void MOVS(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    state->ST = state->V[x];
+    state->PC += 2;
+}
+void MOVR(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    for (int i = 0; i <= x; i++) { state->memory[state->I + i] = state->V[i]; }
+    state->I += (x + 1);
+    state->PC += 2;
+}
+void MOVM(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    for (int i = 0; i <= x; i++) { state->V[i] = state->memory[state->I + i]; }
+    state->I += (x + 1);
+    state->PC += 2;
+}
+void LOADD(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    state->V[x] = state->DT;
+    state->PC += 2;
+}
+void MOVBCD(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = state->V[GET_X(opCode)];
+    state->memory[state->I] = x / 100;
+    state->memory[state->I + 1] = x / 10 % 10;
+    state->memory[state->I + 2] = x % 10;
+    state->PC += 2;
+};
+
+void ADI(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    state->I += state->V[x];
+    state->PC += 2;
+}
+void ADD(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t kk = GET_KK(opCode);
+    state->V[x] = state->V[x] + kk;
+    state->PC += 2;
+}
+void AND(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t y = GET_Y(opCode);
+    state->V[x] &= state->V[y];
+    state->PC += 2;
+}
+void OR(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t y = GET_Y(opCode);
+    state->V[x] |= state->V[y];
+    state->PC += 2;
+}
+void XOR(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t y = GET_Y(opCode);
+    state->V[x] ^= state->V[y];
+    state->PC += 2;
+}
+void SHR(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t LSB = state->V[x] & 0x01;  // List Significant Bit
+    state->V[x] = state->V[x] >> 1;
+    state->V[0xf] = LSB;
+    state->PC += 2;
+}
+void SHL(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t MSB = state->V[x] >> 7;  // Most Significant Bit
+    state->V[x] = state->V[x] << 1;
+    state->V[0xf] = MSB;
+    state->PC += 2;
+}
+void RAND(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t kk = GET_KK(opCode);
+    state->V[x] = rand() % 255 + kk;
+    state->PC += 2;
+}
+void ADD_REG(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t y = GET_Y(opCode);
+    const uint16_t res = state->V[x] + state->V[y];
+    state->V[x] = res & 0x00ff;
+    state->V[0xf] = res > 0xff;
+    state->PC += 2;
+}
+void SUB_REG(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t y = GET_Y(opCode);
+    const uint8_t res = state->V[x] - state->V[y];
+    const uint8_t borrow = state->V[x] >= state->V[y];
+    state->V[x] = res;
+    state->V[0xf] = borrow;
+    state->PC += 2;
+}
+void SUBN_REG(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    const uint8_t y = GET_Y(opCode);
+    state->V[x] = state->V[y] - state->V[x];
+    state->V[0xf] = state->V[y] > state->V[x];
+    state->PC += 2;
+}
+
+void DRAW(Chip8State *state, uint8_t *opCode) {
+    const uint8_t X = state->V[GET_X(opCode)] % DISPLAY_WIDTH;
+    const uint8_t Y = state->V[GET_Y(opCode)] % DISPLAY_HEIGHT;
+    const uint8_t n = (GET_KK(opCode) & 0x0f);
+
+    state->V[0xf] = 0;
+    for (int i = 0; i < n; i++) {
+        const uint8_t pixel = state->memory[state->I + i];
+        const uint8_t rowCord = (Y + i);
+        if (rowCord >= DISPLAY_HEIGHT) break;
+
+        for (int j = 0; j < 8; j++) {
+            const uint8_t colCord = (X + j);
+            if (colCord >= DISPLAY_WIDTH) break;
+            if (pixel & (0x80 >> j)) {
+                state->V[0xf] = state->FB[rowCord][colCord];
+                state->FB[rowCord][colCord] ^= 1;
+            }
+        }
+    }
+    state->PC += 2;
+}
+void SPRITE(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    printf("\n============= %x  %x ===========\n", state->V[x],
+           FONT_ADDR + state->V[x] * 5);
+    state->I = FONT_ADDR + state->V[x] * 5;
+    state->PC += 2;
+}
+void KEY_PRS(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    if (state->keys[state->V[x]]) state->PC += 2;
+    state->PC += 2;
+}
+void KEY_NPRS(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    if (!state->keys[state->V[x]]) state->PC += 2;
+    state->PC += 2;
+}
+void KEY_WAIT(Chip8State *state, uint8_t *opCode) {
+    const uint8_t x = GET_X(opCode);
+    for (int i = 0; i < 16; i++) {
+        if (state->keys[i]) {
+            state->V[x] = state->keys[i];
+            state->PC += 2;
+            return;
+        }
+    }
+}
--- src/main.c
+++ src/main.c
@@ -6,6 +6,27 @@
 #define SCALED_HEIGHT (SCALE * DISPLAY_HEIGHT)
 #define SCALED_WIDTH (SCALE * DISPLAY_WIDTH)
 
+const Color dark = {0xa8, 0x76, 0x2a, 0xff};
+const Color light = {255, 220, 70, 255};
+const KeyboardKey keys[16] = {
+    KEY_X,      // 0
+    KEY_ONE,    // 1
+    KEY_TWO,    // 2
+    KEY_THREE,  // 3
+    KEY_Q,      // 4
+    KEY_W,      // 5
+    KEY_E,      // 6
+    KEY_A,      // 7
+    KEY_S,      // 8
+    KEY_D,      // 9
+    KEY_Z,      // A
+    KEY_C,      // B
+    KEY_FOUR,   // C
+    KEY_R,      // D
+    KEY_F,      // E
+    KEY_V       // F
+};
+
 int main(const int argc, char* argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <path to CHIP-8 rom>\n", argv[0]);
@@ -15,17 +36,46 @@ int main(const int argc, char* argv[]) {
     Chip8State* state = InitChip8();
     LoadRom(state, argv[1]);
     InitWindow(SCALED_WIDTH, SCALED_HEIGHT, "CHIP-8 EMULATOR");
+    printf("\n====================================================\n");
 
+    const float timeStep = 1.0f / 60.0f;  // 60 Instructions per second
+    float currentTime = GetTime();
+    float accumulator = 0.0f;
     while (state->running) {
         if (WindowShouldClose()) state->running = 0;
+        const float newTime = GetTime();
+        const float frameTime = newTime - currentTime;
+        currentTime = newTime;
+        accumulator += frameTime;
 
-        // Draw screen
+        for (uint8_t i = 0; i < 16; i++) {
+            state->keys[i] = IsKeyDown(keys[i]) ? 1 : 0;
+        }
+
+        while (accumulator >= timeStep) {
+            for (int i = 0; i < 10; i++) EmulateCycle(state);
+            accumulator -= timeStep;  // Subtract fixed time step
+
+            // Update timers
+            if (state->DT > 0) { state->DT--; }
+            if (state->ST > 0) { state->ST--; }
+        }
+
+        // Render the current frame
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(dark);
+
+        for (int y = 0; y < 32; y++) {
+            for (int x = 0; x < 64; x++) {
+                const Color color = state->FB[y][x] ? light : dark;
+                DrawRectangle(x * SCALE, y * SCALE, SCALE, SCALE, color);
+            }
+        }
         EndDrawing();
-        // emulate and render cycles
     }
 
+    // Cleanup
+    free(state);
     CloseWindow();
     return EXIT_SUCCESS;
 }
